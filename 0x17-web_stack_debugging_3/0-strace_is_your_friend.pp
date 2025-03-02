16dfffaf99a8446e2a79219ba1b565b2c29c7e8a
DzeBakuEsoc <dzeblebaku@gmail.com>
2025-03-01 12:47:25 -1200

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,12 +2,12 @@
 
 # Ensure PHP MySQL module is installed
 package { 'php-mysql':
-  ensure => installed,
+    ensure => installed,
 }
 
 # Ensure Apache service is running
 service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => Package['php-mysql'],
+    ensure  => running,
+    enable  => true,
+    require => Package['php-mysql'],
 }
