2e1436b611d60490eae2572b452fb6966548c511
DzeBakuEsoc <dzeblebaku@gmail.com>
2025-03-01 12:36:51 -1200

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,20 +1,19 @@
-# Puppet manifest to fix Apache 500 error
+# Puppet manifest to fix Apache 500 error for WordPress
 
-exec { 'install_php_module':
-  command => '/usr/bin/apt-get install -y php libapache2-mod-php',
-  path    => ['/usr/bin', '/usr/sbin'],
-  onlyif  => 'test ! -f /usr/lib/apache2/modules/libphp7.so',
+class fix_apache_500 {
+  
+  # Ensure PHP MySQL module is installed
+  package { 'php-mysql':
+    ensure => installed,
+  }
+
+  # Ensure Apache service is running
+  service { 'apache2':
+    ensure    => running,
+    enable    => true,
+    require   => Package['php-mysql'],
+  }
 }
 
-exec { 'set_permissions':
-  command => '/bin/chown -R www-data:www-data /var/www/html && /bin/chmod -R 755 /var/www/html',
-  path    => ['/bin', '/usr/bin'],
-  onlyif  => 'test $(stat -c "%U" /var/www/html) != "www-data"',
-}
-
-exec { 'restart_apache':
-  command => '/usr/sbin/service apache2 restart',
-  path    => ['/usr/sbin', '/bin', '/usr/bin'],
-  subscribe => Exec['install_php_module'],
-}
-
+# Apply the class
+include fix_apache_500
