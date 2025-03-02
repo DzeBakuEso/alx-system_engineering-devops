1580152496999d70a4ea7110a32438c06d16299f
DzeBakuEsoc <dzeblebaku@gmail.com>
2025-03-01 12:44:02 -1200

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,20 +1,13 @@
 # Puppet manifest to fix Apache 500 error for WordPress
 
-class fix_apache_500 {
-  
-  # Ensure PHP MySQL module is installed
-  package { 'php-mysql':
-    ensure => installed,
-  }
-
-  # Ensure Apache service is running
-  service { 'apache2':
-    ensure    => running,
-    enable    => true,
-    require   => Package['php-mysql'],
-  }
+# Ensure PHP MySQL module is installed
+package { 'php-mysql':
+  ensure => installed,
 }
 
-# Apply the class
-include fix_apache_500
-
+# Ensure Apache service is running
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  require => Package['php-mysql'],
+}
